c5741c2b11fc6dd078b1cb71782f0a4e2332216d
GitHub Action <action@github.com>
2022-04-04 03:29:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/liangyp1994/grpcjavautil/MyBundle.kt
+++ src/main/kotlin/com/github/liangyp1994/grpcjavautil/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.liangyp1994.grpcjavautil
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/liangyp1994/grpcjavautil/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/liangyp1994/grpcjavautil/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.liangyp1994.grpcjavautil.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liangyp1994.grpcjavautil.MyBundle
 
 class MyApplicationService {
 
